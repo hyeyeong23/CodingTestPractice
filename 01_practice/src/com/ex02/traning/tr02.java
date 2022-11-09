2ef2576910742059295d06e4ad531be097b22b4e
DESKTOP-2EE09HS\nghy2 <nghy90@gmail.com>
2022-11-09 22:26:03 +0900

    221109 배열 연습문제 풀이 완료
    1. 배열에서 최댓값 구하기
    2. 배열에서 초기화하면서 선언하기

---
+++ 01_practice/src/com/ex02/traning/tr02.java
@@ -0,0 +1,14 @@
+package com.ex02.traning;
+
+public class tr02 {
+
+	/* 배열을 초기화하며 선언하기 */
+	public static void main(String[] args) {
+		int[] a = {1, 2, 3, 4, 5};
+		
+		for(int i = 0; i < a.length; i++) {
+			System.out.println("a[" + i + "] : " + a[i]);
+		}
+	}
+
+}
