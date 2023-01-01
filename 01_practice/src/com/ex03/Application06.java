3d18d43679ca5429a1dbcfc286c67b48398ae2dd
NamGungHyeYeong <nghy90@gmail.com>
2023-01-01 20:25:26 +0900

    230101 연습문제 6번 풀이 추가
    검색에 실패할 때 삽입 포인트의 값 출력 추가

--- 01_practice/src/com/ex03/Application06.java
+++ 01_practice/src/com/ex03/Application06.java
@@ -31,7 +31,10 @@ public class Application06 {
 		
 		/* 검색에 실패할 때 삽입 포인트의 값 출력 추가 */
 		if(index < 0) {
+			int point = -index - 1;
 			System.out.println("그 값의 요소가 없습니다.");
+			System.out.println("삽입 포인트는 " + point + "입니다.");
+			System.out.println("x[" + point + "]의 바로 앞에 " + key + "를 삽입하면 배열의 정렬이 유지됩니다.");
 		} else {
 			System.out.println("그 값은 x[" + index + "]에 있습니다.");
 		}
