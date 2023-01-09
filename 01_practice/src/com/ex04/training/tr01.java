4540134363ddfb30e92e9c621bb37b6652448733
NamGungHyeYeong <nghy90@gmail.com>
2023-01-09 20:46:12 +0900

    230109 스택 관련 메소드

--- 01_practice/src/com/ex04/training/tr01.java
+++ 01_practice/src/com/ex04/training/tr01.java
@@ -58,4 +58,26 @@ public class tr01 {
 		return capacity;
 	}
 
+	public int size() {
+		return ptr;
+	}
+	
+	public boolean isEmpty() {
+		return ptr <= 0;
+	}
+	
+	public boolean isFull() {
+		return ptr >= capacity;
+	}
+	
+	public void dump() {
+		if(ptr <= 0) {
+			System.out.println("스택이 비어 있습니다.");
+		} else {
+			for(int i = 0; i < ptr; i++) {
+				System.out.print(stk[i] + " ");
+			}
+			System.out.println();
+		}
+	}
 }
