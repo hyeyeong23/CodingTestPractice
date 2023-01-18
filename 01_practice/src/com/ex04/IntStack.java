2892cd2b43f55f088bdcb8c740533812ef991025
NamGungHyeYeong <nghy90@gmail.com>
2023-01-18 21:44:45 +0900

    230118 문제에 주석 추가 작성

--- 01_practice/src/com/ex04/IntStack.java
+++ 01_practice/src/com/ex04/IntStack.java
@@ -5,51 +5,59 @@ public class IntStack {
 	private int capacity;		// 스택 용량
 	private int ptr;			// 스택 포인터
 	
+	/* 실행 시 예외 : 스택이 비어 있음 */
 	public class EmptyIntStackException extends RuntimeException {
 		public EmptyIntStackException() {}
 	}
 	
+	/* 실행 시 예외 : 스택이 가득 참 */
 	public class OverflowIntStackException extends RuntimeException {
 		public OverflowIntStackException() {}
 	}
 	
+	/* 생성자 */
 	public IntStack(int maxlen) {
 		ptr = 0;
 		capacity = maxlen;
 		try {
-			stk = new int[capacity];
-		} catch (OutOfMemoryError e) {
+			stk = new int[capacity];		// 스택 본체용 배열을 생성
+		} catch (OutOfMemoryError e) {		// 생성할 수 없음
 			capacity = 0;
 		}
 	}
 	
+	/* 스택에 x를 푸시 */
 	public int push(int x) throws OverflowIntStackException {
-		if(ptr >= capacity) {
+		if(ptr >= capacity) {			// 스택이 가득 차있음
 			throw new OverflowIntStackException();
 		}
 		return stk[ptr++] = x;
 	}
 	
+	/* 스택에서 데이터를 팝함 */
 	public int pop() throws EmptyIntStackException {
-		if(ptr <= 0) {
+		if(ptr <= 0) {					// 스택이 비어 있음
 			throw new EmptyIntStackException();
 		}
 		return stk[--ptr];
 	}
 	
+	/* 스택에서 데이터를 피크함 */
 	public int peek() throws EmptyIntStackException {
-		if(ptr <= 0) {
+		if(ptr <= 0) {					// 스택이 비어 있음
 			throw new EmptyIntStackException();
 		}
 		return stk[ptr - 1];
 	}
 	
+	/* 스택을 비움 */
 	public void clear() {
 		ptr = 0;
 	}
 	
+	/* 스택에서 x를 찾아 인덱스를 반환 */
 	public int indexOf(int x) {
-		for(int i = ptr - 1; i >= 0; i--) {
+		for(int i = ptr - 1; i >= 0; i--) {		// 꼭대기부터 선형 검색
 			if(stk[i] == x) {
 				return i;
 			}
@@ -57,22 +65,27 @@ public class IntStack {
 		return -1;
 	}
 	
+	/* 스택의 용량을 반환 */
 	public int getCapacity() {
 		return capacity;
 	}
 	
+	/* 스택에 쌓여 있는 데이터 개수를 반환 */
 	public int size() {
 		return ptr;
 	}
 	
+	/* 스택이 비어 있는지 확인 */
 	public boolean isEmpty() {
 		return ptr <= 0;
 	}
 	
+	/* 스택이 가득 차 있는지 확인 */
 	public boolean isFull() {
 		return ptr >= capacity;
 	}
 	
+	/* 스택 안의 모든 데이터를 바닥부터 꼭대기 순서로 출력 */
 	public void dump() {
 		if(ptr <= 0) {
 			System.out.println("스택이 비어 있습니다.");
