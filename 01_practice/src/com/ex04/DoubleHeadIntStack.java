136afb679261ba02fc41a394c0d2f88017d57511
NamGungHyeYeong <nghy90@gmail.com>
2023-01-19 22:22:35 +0900

    230119 스택, 큐 관련 문제 풀이 진행중

--- 01_practice/src/com/ex04/DoubleHeadIntStack.java
+++ 01_practice/src/com/ex04/DoubleHeadIntStack.java
@@ -1,21 +1,24 @@
 package com.ex04;
 
 public class DoubleHeadIntStack {
-	private int[] stk;
-	private int capacity;
-	private int ptrA;
-	private int ptrB;
+	private int[] stk;			// 스택 본체
+	private int capacity;		// 스택 용량
+	private int ptrA;			// 스택포인터A
+	private int ptrB;			// 스택포인터B
 	
 	public enum AorB {StackA, StackB};
 	
+	/* 실행 시 예외 : 스택이 비어 있음 */
 	public class EmptyDoubleHeadIntStackException extends RuntimeException {
 		public EmptyDoubleHeadIntStackException() {}
 	}
 	
+	/* 실행 시 예외 : 스택이 가득 참 */
 	public class OverflowDoubleHeadIntStackException extends RuntimeException {
 		public OverflowDoubleHeadIntStackException() {}
 	}
 	
+	/* 생성자 */
 	public DoubleHeadIntStack(int maxlen) {
 		ptrA = 0;
 		ptrB = maxlen - 1;
@@ -27,6 +30,7 @@ public class DoubleHeadIntStack {
 		}
 	}
 	
+	/* 스택에 x를 푸시 */
 	public int push(AorB sw, int x) throws OverflowDoubleHeadIntStackException {
 		if(ptrA >= ptrB + 1) {
 			throw new OverflowDoubleHeadIntStackException();
@@ -38,6 +42,7 @@ public class DoubleHeadIntStack {
 		return x;
 	}
 	
+	/* 스택에서 데이터를 팝 */
 	public int pop(AorB sw) throws EmptyDoubleHeadIntStackException {
 		int x = 0;
 		switch (sw) {
@@ -55,5 +60,46 @@ public class DoubleHeadIntStack {
 			break;
 		}
 		return x;
+	}
+	
+	/* 스택에서 데이터를 피크 */
+	public int peek(AorB sw) throws EmptyDoubleHeadIntStackException {
+		int x = 0;
+		switch (sw) {
+		case StackA:
+			if(ptrA <= 0) {
+				throw new EmptyDoubleHeadIntStackException();
+			}
+			x = stk[ptrA - 1];
+			break;
+		case StackB:
+			if(ptrB >= capacity - 1) {
+				throw new EmptyDoubleHeadIntStackException();
+			}
+			x = stk[ptrB + 1];
+			break;
+		}
+		return x;
+	}
+	
+	/* 스택에서 x를 검색하여 인덱스를 반환(발견하지 못하면 -1을 반환) */
+	public int indexOf(AorB sw, int x) {
+		switch (sw) {
+		case StackA:
+			for(int i = ptrA - 1; i >= 0; i--) {
+				if(stk[i] == x) {
+					return i;
+				}
+			break;
+			}
+		case StackB:
+			for(int i = ptrB + 1; i < capacity; i++) {
+				if(stk[i] == x) {
+					return i;
+				}
+			break;
+			}
+		}
+		return -1;
 	}
 }
