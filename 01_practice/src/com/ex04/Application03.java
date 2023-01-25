a0d00c49bd3b8a891c7cc9da40f4fe500f9d63d0
NamGungHyeYeong <nghy90@gmail.com>
2023-01-25 21:57:42 +0900

    230125 스택과 큐 관련 문제에서 case1, 2번 작성 완료

---
+++ 01_practice/src/com/ex04/Application03.java
@@ -0,0 +1,46 @@
+package com.ex04;
+
+import java.util.Scanner;
+
+public class Application03 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		DoubleHeadIntStack s = new DoubleHeadIntStack(64);	// 최대 64개를 푸시할 수 있는 머리가 둘인 스택
+		
+		while(true) {
+			System.out.println("현재 데이터 개수:   A: " + s.size(DoubleHeadIntStack.AorB.StackA) + "/"
+					+ "B: " + s.size(DoubleHeadIntStack.AorB.StackB));
+			System.out.print("(1) A푸시  (2) A팝  (3) A피크  (4) A덤프  (5) A검색  (6) A비움 \n" +
+					"(7) B푸시  (8) B팝  (9) B피크  (10) B덤프  (11) B검색  (12) B비움\n" +
+					"(13) 출력  (0) 종료 : ");
+			
+			int menu = sc.nextInt();
+			if(menu == 0) break;
+			
+			int n = 0;
+			int x = 0;
+			
+			switch(menu) {
+				case 1:
+					System.out.print("데이터 : ");
+					x = sc.nextInt();
+					try {
+						s.push(DoubleHeadIntStack.AorB.StackA, x);
+					} catch (DoubleHeadIntStack.EmptyDoubleHeadIntStackException e) {
+						System.out.println("스택이 가득 찼습니다.");
+					}
+					break;
+				case 2:
+					try {
+						x = s.pop(DoubleHeadIntStack.AorB.StackA);
+						System.out.println("팝한 데이터는 " + x + "입니다.");
+					} catch(DoubleHeadIntStack.EmptyDoubleHeadIntStackException e) {
+						System.out.println("스택이 비어 있습니다.");
+					}
+					break;
+			}
+		}
+	}
+
+}
