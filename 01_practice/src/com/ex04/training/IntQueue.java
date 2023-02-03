1c28b229fc9f8f48441307494fcf46c38ab12071
NamGungHyeYeong <nghy90@gmail.com>
2023-02-03 21:57:39 +0900

    230203 intQueue 주석 추가

--- 01_practice/src/com/ex04/training/IntQueue.java
+++ 01_practice/src/com/ex04/training/IntQueue.java
@@ -1,26 +1,29 @@
 package com.ex04.training;
 
 public class IntQueue {
-	private int[] que;
-	private int capacity;
-	private int front;
-	private int rear;
-	private int num;
+	private int[] que;		// 큐용 배열
+	private int capacity;	// 큐의 용량
+	private int front;		// 맨 앞의 요소 커서
+	private int rear;		// 맨 뒤의 요소 커서
+	private int num;		// 현재 데이터 개수
 	
+	/* 실행 시 예외:큐가 비어 있음 */
 	public class EmptyIntQueueException extends RuntimeException {
 		public EmptyIntQueueException() {}
 	}
 	
+	/* 실행 시 예외: 큐가 가득 참 */
 	public class OverflowIntQueueException extends RuntimeException {
 		public OverflowIntQueueException() {}
 	}
 	
+	/* 생성자 */
 	public IntQueue(int maxlen) {
 		num = front = rear = 0;
 		capacity = maxlen;
 		try {
-			que = new int[capacity];
-		} catch (OutOfMemoryError e) {
+			que = new int[capacity];		// 큐 본체용 배열을 생성
+		} catch (OutOfMemoryError e) {		// 생성할 수 없음
 			capacity = 0;
 		}
 	}
