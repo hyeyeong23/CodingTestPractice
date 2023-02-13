30f50258998d86f4abf0a441085574ce738c1f0c
NamGungHyeYeong <nghy90@gmail.com>
2023-02-13 21:43:58 +0900

    230213 프로그래머스 치킨 쿠폰 문제 풀이 완료

--- 02_programmers/src/com/practice01/practice07.java
+++ 02_programmers/src/com/practice01/practice07.java
@@ -3,6 +3,8 @@ package com.practice01;
 public class practice07 {
 	
 	String my_string = "3 + 4";
+	int chicken = 100;
+	int result = 11;
 	
 	/* 문자열 계산하기 */
 	public int solution(String my_string) {
@@ -19,4 +21,19 @@ public class practice07 {
         }
         return answer;
     }
+	
+	/* 치킨 쿠폰 */
+    public int solution(int chicken) {
+        int answer = 0;
+        int coupon = chicken;
+        
+        while(coupon >= 10) {
+            answer += coupon / 10;              // 최대 서비스 치킨의 수
+            int rest = coupon % 10;             // 남은 쿠폰수
+            coupon = rest + (coupon / 10);      // 총 쿠폰 수 = 남는 쿠폰수 + 서비스치킨으로 생긴 쿠폰 수
+        }
+        
+        return answer;
+    }
+
 }
