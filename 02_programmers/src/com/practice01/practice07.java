77c5f506dfca415c63758660851c21737f55c66d
NamGungHyeYeong <nghy90@gmail.com>
2023-02-28 21:08:20 +0900

    230228 프로그래머스 저주의 숫자3 문제 풀이 완료

--- 02_programmers/src/com/practice01/practice07.java
+++ 02_programmers/src/com/practice01/practice07.java
@@ -93,4 +93,18 @@ public class practice07 {
         
         return str;
     }
+    
+    /* 저주의 숫자 3 */
+    public int solution1(int n) {
+        int answer = 0;
+        
+        for(int i = 0; i < n; i++) {
+            answer++;
+            while(answer % 3 == 0 || String.valueOf(answer).contains("3")) {
+              answer++;
+              System.out.println(i + "=" + answer);
+            } 
+        }
+        return answer;
+    }
 }
