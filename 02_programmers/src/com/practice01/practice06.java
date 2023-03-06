81d4bd9d049d0289a5596e7aa9503fb71ef6403f
NamGungHyeYeong <nghy90@gmail.com>
2023-03-06 21:23:15 +0900

    230306 프로그래머스 나머지가 1이 되는 수 찾기 문제 풀이 완료

--- 02_programmers/src/com/practice01/practice06.java
+++ 02_programmers/src/com/practice01/practice06.java
@@ -3,6 +3,7 @@ package com.practice01;
 public class practice06 {
 
 	String[] id_pw = {"meosseugi", "1234"};
+	int n = 10;
 	
 	/* 로그인 성공 문제 풀기 */
 	public String solution(String[] id_pw, String[][] db) {
@@ -22,6 +23,18 @@ public class practice06 {
                }
         }
         
+        return answer;
+    }
+	
+	/* 나머지가 1이 되는 수 찾기 */
+	public int solution(int n) {
+        int answer = 0;
+        
+        for(int i = 2; i <= n; i++) {
+            if(n % i == 1) {
+                return i;
+            }
+        }
         return answer;
     }
 
