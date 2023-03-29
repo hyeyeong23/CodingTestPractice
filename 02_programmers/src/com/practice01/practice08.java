3522c3f2dd2d0844df8df0e60554f5a8a4241ad6
NamGungHyeYeong <nghy90@gmail.com>
2023-03-29 21:26:13 +0900

    230329 하샤드 수 문제 풀이 완료

--- 02_programmers/src/com/practice01/practice08.java
+++ 02_programmers/src/com/practice01/practice08.java
@@ -22,4 +22,24 @@ public class practice08 {
         
         return count;
     }
+	
+	/* 하샤드 수 */
+	public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+        
+        String str = Integer.toString(x);
+        int[] arrNum = new int[str.length()];
+        for(int i = 0; i < str.length(); i++) {
+            arrNum[i] = str.charAt(i) - '0';
+        }
+        
+        for(int i = 0; i < arrNum.length; i++) {
+            sum += arrNum[i];
+        }
+        if(x % sum != 0) {
+            answer = false;
+        }
+        return answer;
+    }
 }
